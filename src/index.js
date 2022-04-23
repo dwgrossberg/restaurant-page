05f494540848bb46dbdb3e6ed8dbd16033bced10
Dan Grossberg <dwgrossberg@gmail.com>
2022-04-23 13:21:49 +0700

    Setup basic homepage style foundation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
 import './style.css';
-import githubImg from './assets/github.png';
-
-const githubImgDOM = document.getElementById('githubImg');
-const githubIcon = new Image();
-githubIcon.src = githubImg;
-
-
 
